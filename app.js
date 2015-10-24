bd419828f3acbd13f056d008cf5794d7e5fe31f6
easydaniel <whitetiger50512@gmail.com>
2015-10-24 15:56:39 +0800

    auth

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session      = require('express-session');
+var passport = require('passport');
+
 
 var routes = require('./routes/index');
 var auth = require('./routes/auth');
@@ -23,6 +26,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'QQQQQQsomeRandom',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/auth', auth);
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,63 @@
  * Created by Daniel on 2015/10/24.
  */
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('login', { title: 'Auth' });
+router.get('/', function (req, res, next) {
+    res.render('login', {title: 'Auth'});
 });
 
+router.post('/login', function (req, res, next) {
+    var username = req.body.username;
+    var passwd = req.body.passwd;
+    var domain = req.body.domain;
+
+    var payload = {
+        'auth': {
+            'identity': {
+                'methods': ['password'],
+                'password': {
+                    'user': {
+                        'domain': {
+                            'name': domain
+                        },
+                        'name': username,
+                        'password': passwd
+                    }
+                }
+            },
+            'scope': {
+                'project': {
+                    'domain': {
+                        'name': domain
+                    },
+                    'name': username
+                }
+            }
+        }
+    };
+
+    // Set the headers
+    var headers = {
+        'User-Agent': username + '.User.Portal'
+    };
+
+// Configure the request
+    var options = {
+        url: 'http://hackathon.promise.com.tw/keystone/v3/auth/tokens',
+        method: 'POST',
+        headers: headers,
+        json: payload
+    };
+
+// Start the request
+    request(options, function (error, response, body) {
+        res.send(response);
+    });
+
+});
 
 
 module.exports = router;
