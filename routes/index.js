b54677a5e8cc3e2c63739f8f79ec6996b0c2b337
easydaniel <whitetiger50512@gmail.com>
2015-10-25 04:18:15 +0800

    better db

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,90 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
+
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 var Event = new Schema({
-    user_id    : String,
-    content    : String,
-    updated_at : Date
-});
+        name: String,
+        organization: String,
+        area: String,
+        country: String,
+        img_scr: String,
+        title: String,
+        date: String,
+        time: String,
+        location: String,
+        host: String,
+        price: Number,
+        sponsor: String,
+        quota: Number,
+        detail: String,
+        tags: [
+                {
+                    name: String
+                }
+            ]
+    }
+);
 
 var event = mongoose.model('event', Event);
 
+obj = {
+    countries:[
+        {
+            name:"Taiwan",
+            areas:[
+                {
+                    name:"Hsinchu",
+                    organizations:[
+                        {
+                            name:"NCTU"
+                        }
+                    ]
+                },
+                {
+                    name:"Kaoshiung",
+                    organizations:[
+                    ]
+                },
+                {
+                    name:"Taipei",
+                    "organizations":[
+                    ]
+                },
+                {
+                    name:"Yilan",
+                    organizations:[
+                    ]
+                },
+                {
+                    name:"Taichung",
+                    organizations:[
+                    ]
+                }
+            ]
+        },
+        {
+            name:"US",
+            areas:[
+            ]
+        }
+
+    ]
+};
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    event.find({}, function (err, result) {
-        console.log(result);
+    event.find({}).limit(8).exec(function (err, result) {
+        res.render('index', {
+            title: 'NCTU Act',
+            token: '',
+            login: req.session.token !== undefined,
+            user: req.session.user,
+            data: result,
+            struct: obj
+        });
     });
-    res.render('index', {
-        title: 'NCTU Act',
-        token: '',
-        login: req.session.token !== undefined,
-        user: req.session.user
-    });
-
 });
 
 
