bcbaaf8663edda584efb76fea3d5685806b91b15
easydaniel <whitetiger50512@gmail.com>
2015-10-25 10:05:09 +0800

    better db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+var fs = require('fs');
 
 
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
+var random = require('mongoose-simple-random');
 var Event = new Schema({
         name: String,
         organization: String,
@@ -27,7 +29,7 @@ var Event = new Schema({
             ]
     }
 );
-
+Event.plugin(random);
 var event = mongoose.model('event', Event);
 
 obj = {
@@ -121,7 +123,7 @@ obj = {
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    event.find({}).limit(8).exec(function (err, result) {
+    event.findRandom({}, {}, {limit: 8}, function(err, result) {
         res.render('index', {
             title: 'NCTU Act',
             token: '',
@@ -133,11 +135,37 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.get('/:filter', function (req, res, next) {
-    console.log(req.params);
-    event.find({}).limit(8).exec(function (err, result) {
-        res.send(result);
-    });
+//router.get('/:filter', function (req, res, next) {
+//    console.log(req.params);
+//    event.find({}).limit(8).exec(function (err, result) {
+//        res.send(result);
+//    });
+//});
+
+router.get('/upload', function (req, res, next) {
+    var time = Date.now();
+    var f = fs.readFileSync('auth.js');
+    var options = {
+        url: 'http://hackathon.promise.com.tw/fileop/v1/files/',
+        method: 'GET',
+        headers: {
+            'X-Auth-Token' : req.session.token,
+            'User-Agent': req.session.header,
+            'X-Meta-FC-Compress': false,
+            'X-Meta-FC-Encrypt': false,
+            'X-Meta-FC-Mtime': time+"#"+time+"#"+time
+        },
+        postData: {
+            params: {
+                overite: true
+            }
+        },
+        body: f
+    };
+    request(options, function (err, response, body) {
+        console.log(response);
+        res.redirect('/');
+    })
 });
 
 
